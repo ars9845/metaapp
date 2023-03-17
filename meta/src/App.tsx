cfc289e857e2b2fddf945fc0682c12e32b80dd45
ars9845 <ars9845@naver.com>
2023-03-17 16:23:32 +0900

    - walletconnet v1 추가 - qr코드 생성 - npm buffer 추가

--- meta/src/App.tsx
+++ meta/src/App.tsx
@@ -4,7 +4,9 @@ import {
 } from 'wagmi'
 import { infuraProvider } from 'wagmi/providers/infura'
 import { publicProvider } from 'wagmi/providers/public'
-import { MetaMaskConnector } from 'wagmi/connectors/metaMask' 
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect' 
+import { WalletConnectLegacyConnector } from '@wagmi/core/connectors/walletConnectLegacy'
 import Connectors from './views/connectors/Connectors'
 
 import "./app.scss"
@@ -13,16 +15,37 @@ const { chains, provider, webSocketProvider } = configureChains(
   [mainnet, goerli],
   [infuraProvider({ apiKey: '0002457bd5404ab3a48c9f35b76a4eb6' }), publicProvider()],
 )
+
+window.Buffer = window.Buffer || require("buffer").Buffer;
  
-console.log(chains);
 // Set up client
 const client = createClient({
   autoConnect: true,
   connectors: [
-    new MetaMaskConnector({ chains })    
+    new MetaMaskConnector({ chains }),
+    // new WalletConnectConnector({
+    //   chains,      
+    //   options: {
+    //     projectId: '0018915e8b4555922af90751e0354527',
+    //     showQrModal: true,        
+    //   },
+    // }), 
+    new WalletConnectLegacyConnector({
+      chains,      
+      options: {        
+        qrcode: true,        
+      },
+    }),    
   ],
   provider,
   webSocketProvider,
+})
+
+const connector = new WalletConnectLegacyConnector({
+  chains: [mainnet, goerli],
+  options: {
+    qrcode: true,
+  },
 })
  
 // Pass client to React Context Provider
--- meta/src/views/connectors/Connectors.tsx
+++ meta/src/views/connectors/Connectors.tsx
@@ -12,11 +12,11 @@ import Transaction from '../transaction/Transaction'
 import "./connectors.scss"
  
 export function Connectors() {
-  const { address, connector, isConnected } = useAccount()
-  // const { data: ensAvatar } = useEnsAvatar({ address: '0xA0Cf798816D4b9b9866b5330EEa46a18382f251e', })  
-  // const { data: ensName } = useEnsName({ address })
+  const { address, connector, isConnected } = useAccount()  
+  const { data: ensAvatar } = useEnsAvatar({ address })
+  const { data: ensName } = useEnsName({ address })
   const { connect, connectors, error, isLoading, pendingConnector } = useConnect()
-  
+  const { disconnect } = useDisconnect()
 
   if (isConnected) {
     return (
@@ -25,8 +25,9 @@ export function Connectors() {
   }
  
   return (
-    <div className="content">
-      {connectors.map((connector) => (                
+    <div className="connect-content">
+      {connectors.map((connector, idx) => (                
+        
         <button        
           className={isLoading ? "btn-connect isload" : "btn-connect"}
           style={{"--clr": "#1149ff"} as React.CSSProperties}          
@@ -43,9 +44,10 @@ export function Connectors() {
           </span>
           <i></i>                          
         </button>
+        
       ))}
  
-      {error && <div>{error.message}</div>}
+      {error && <div className="err-txt">{error.message}</div>}
     </div>              
   )
 }
